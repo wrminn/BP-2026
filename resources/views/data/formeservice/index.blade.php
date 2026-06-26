f4704c3059e1816055ffd473351a92fb80e28b96
warindacontact <warinda.contact@gmail.com>
2026-06-26 18:46:39 +0700

    update 20260626

--- resources/views/data/formeservice/index.blade.php
+++ resources/views/data/formeservice/index.blade.php
@@ -5,14 +5,14 @@
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.css" rel="stylesheet">
     <div class="text-center ">
         <div class="relative my-10 inline-block h-34" style="width: 594px">
-            <img alt="{{ $title }}"class="w-full h-full object-contain" src="/spa/assets/list/label-frame.png">
+            {{-- <img alt="{{ $title }}"class="w-full h-full object-contain" src="/img/label-frame.png"> --}}
             <span
                 class="absolute inset-0 flex items-center justify-center text-[1.6rem] font-bold text-white drop-shadow-lg px-4 text-center">{{ $title }}</span>
         </div>
     </div>
     <section class="b-detail">
         <div class="form-wrapper">
-            <img src="{{ asset('/storage/detailweb/logo.png') }}" alt="Logo" class="d-block mx-auto mb-3"
+            <img src="{{ asset('/img/logo.png') }}" alt="Logo" class="d-block mx-auto mb-3"
                 style="max-width: 150px;">
             <h4 class="mb-4 text-center">เทศบาลตำบลบ้านโพธิ์</h4>
             <div class="list-group">
@@ -24,7 +24,7 @@
                 @endif
             </div>
             <div class="d-flex justify-content-center align-items-center text-center mt-3">
-                <a href="{{ asset('/คู่มือ E-service.pdf') }}" class="btn btn-primary" target="_black">
+                <a href="{{ asset('/img/E-service.pdf') }}" class="btn btn-primary" target="_black">
                     คู่มือการใช้งานระบบ E-Service
                     <i class="bi bi-file-earmark-pdf"></i>
                 </a>
